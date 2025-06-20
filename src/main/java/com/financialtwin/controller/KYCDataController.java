7d89ee94a52f8007adea951b792fdb39fd35e4d1
ahmed shalash <ashalash38@gmail.com>
2025-06-21 01:03:21 +0300

    update kyc data controller

--- src/main/java/com/financialtwin/controller/KYCDataController.java
+++ src/main/java/com/financialtwin/controller/KYCDataController.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/kyc-data")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://wealthmindai.netlify.app/*")
 public class KYCDataController {
 
     @Autowired
